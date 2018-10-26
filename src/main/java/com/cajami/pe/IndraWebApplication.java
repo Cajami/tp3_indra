7fa0d12728b29b0f53ea2e3714c4d79ebf0791bf
LENOVO B570 <LENOVO B570@192.168.1.16>
2018-10-26 00:22:39 -0500

    configurando el rest

--- src/main/java/com/cajami/pe/IndraWebApplication.java
+++ src/main/java/com/cajami/pe/IndraWebApplication.java
@@ -2,10 +2,6 @@ package com.cajami.pe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class IndraWebApplication {
@@ -13,21 +9,7 @@ public class IndraWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IndraWebApplication.class, args);
 	}
+	
+	
 }
 
-
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()...
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-        return source;
-    }
-}
--- src/main/java/com/cajami/pe/controller/ContratoClienteController.java
+++ src/main/java/com/cajami/pe/controller/ContratoClienteController.java
@@ -1,17 +1,43 @@
 package com.cajami.pe.controller;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.cajami.pe.dao.ContratoDao;
+import com.cajami.pe.service.AD_BuscarContratoEntityService;
+import com.cajami.pe.service.AdendaEntityService;
+import com.cajami.pe.service.ApoderadoEntityService;
+import com.cajami.pe.service.ClienteEntityService;
+import com.cajami.pe.service.ConsultarContratoService;
+import com.cajami.pe.service.ContratoEntityService;
 import com.cajami.pe.service.ContratoService;
+import com.cajami.pe.service.GerenteEntityService;
 
 @RestController
 public class ContratoClienteController {
+	
+	@SuppressWarnings("deprecation")
+	@Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurerAdapter() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**").allowedOrigins("*");
+	            }
+	        };
+	    }
+	 
 	
 	@Autowired
 	private ContratoService contratoService;
@@ -31,17 +57,22 @@ public class ContratoClienteController {
 //    @Path("/validarContrato")
 //    @Consumes({MediaType.APPLICATION_JSON})
 //    @Produces({MediaType.APPLICATION_JSON})
+
+	//    @GetMapping("/greeting")
+
+    //public Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
+
 	@RequestMapping(
 			value="/validarContrato",
-			method=RequestMethod.POST,
-			params = {"primerMensaje", "segundoMensaje"}
+			method=RequestMethod.POST
+			//params = {"primerMensaje", "segundoMensaje"}
 	)
 	//	public ArrayList<ClienteEntity> buscarCliente(@FormParam ("tipoBusqueda") int tipoBusqueda,@FormParam("buscar") String buscar) throws SQLException {
 //@RequestParam(value = "_search") String search, 
 
 	public String  validarContrato(
-			@RequestParam(value = "defecto1") String primerMensaje, 
-			@RequestParam(value = "defecto2") String segundoMensaje) throws SQLException {
+			@RequestParam(required=true) String primerMensaje, 
+			@RequestParam(required=true) String segundoMensaje) throws SQLException {
 		
 
     	//ArrayList<ContratoEntity>
@@ -55,4 +86,209 @@ public class ContratoClienteController {
     
 
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@RequestMapping(value="/buscarCliente",method=RequestMethod.POST)
+	public ArrayList<ClienteEntityService> buscarCliente(@RequestParam(required=true) int tipoBusqueda, @RequestParam(required=true) String buscar) throws SQLException {
+    	return new ContratoDao().recuperarClientes(tipoBusqueda, buscar);
+	}
+    
+	@RequestMapping(value="/buscarContratos",method=RequestMethod.POST)
+	public ArrayList<ContratoEntityService> buscarContratos(@RequestParam(required=true) int codigoCliente, @RequestParam(required=true) Date fechaInicio, @RequestParam(required=true) Date fechaFin) throws SQLException {
+    	return new ContratoDao().recuperarContratosBusqueda(codigoCliente, fechaInicio, fechaFin);
+	}
+        
+	@RequestMapping(value="/buscarGerentesFirmantes",method=RequestMethod.POST)
+	public ArrayList<GerenteEntityService> buscarGerentesFirmantes() throws SQLException {
+    	return new ContratoDao().recuperarGerentesFirmantes();
+	}
+    
+	@RequestMapping(value="/buscarApoderado",method=RequestMethod.POST)
+	public ApoderadoEntityService buscarApoderado(@RequestParam(required=true) String dni) throws SQLException {
+    	return new ContratoDao().buscarApoderadoFirmar(dni);
+	}    
+
+	@RequestMapping(value="/buscarClientexRucDni",method=RequestMethod.POST)
+	public ClienteEntityService buscarClientexRucDni(@RequestParam(required=true) String dniRuc) throws SQLException {
+    	return new ContratoDao().buscarClientexRucDni(dniRuc);
+	}
+    
+	@RequestMapping(value="/buscarClienteFirmar",method=RequestMethod.POST)
+	public ClienteEntityService buscarClienteFirmar(@RequestParam(required=true) String dni) throws SQLException {
+    	return new ContratoDao().buscarClienteFirmar(dni);
+	}
+    
+	@RequestMapping(value="/buscarInformacionContrato",method=RequestMethod.POST)
+	public ContratoEntityService buscarInformacionContrato(@RequestParam(required=true) int codigoDistrito, @RequestParam(required=true) int tipoContrato, @RequestParam(required=true) Date fechaInicio, @RequestParam(required=true) String dniRucCliente) throws SQLException {
+    	System.err.println("llegó al servicio");
+    	return new ContratoDao().buscarInformacionContrato(codigoDistrito,tipoContrato,fechaInicio,dniRucCliente);
+	}
+        
+	@RequestMapping(value="/registrarContrato",method=RequestMethod.POST)
+	public int registrarContrato(
+			@RequestParam(required=true) String dniRucCliente,
+			@RequestParam(required=true) int codigoDistrito,
+			@RequestParam(required=true) int tipoContrato,
+			@RequestParam(required=true) int codigoTipoServicio,
+			@RequestParam(required=true) String nombreContrato,
+			@RequestParam(required=true) int numeroContrato,
+			@RequestParam(required=true) String nombreProyecto,
+			@RequestParam(required=true) int tipoMoneda,
+			@RequestParam(required=true) Double montoContractual,
+			@RequestParam(required=true) Date fechaInicio,
+			@RequestParam(required=true) Date fechaFin,
+			@RequestParam(required=true) String clienteFirmante,
+			@RequestParam(required=true) String dniApoderado,
+			@RequestParam(required=true) String dniGerente,
+			@RequestParam(required=true) int numeroPagina,
+			@RequestParam(required=true) int numeroClausula) throws SQLException {
+    	
+    	return new ContratoDao().registrarContrato(
+    			dniRucCliente,
+    			codigoDistrito,
+    			tipoContrato,
+    			codigoTipoServicio,
+    			nombreContrato,
+    			numeroContrato,
+    			nombreProyecto,
+    			tipoMoneda,
+    			montoContractual,
+    			fechaInicio,
+    			fechaFin,
+    			clienteFirmante,
+    			dniApoderado,
+    			dniGerente,
+    			numeroPagina,
+    			numeroClausula);
+	}
+    
+	@RequestMapping(value="/registrarAncedente",method=RequestMethod.POST)
+	public int registrarAncedente(@RequestParam(required=true) int codigoAdenda, @RequestParam(required=true) String descripcion) throws SQLException {
+    	return new ContratoDao().registrarAncedente(codigoAdenda,descripcion);
+	}
+
+    
+	@RequestMapping(value="/registrarCronograma",method=RequestMethod.POST)
+	public int registrarCronograma(
+			@RequestParam(required=true) int codigoAdenda,
+			@RequestParam(required=true) int numeroEntrega,
+			@RequestParam(required=true) String descripcionEntrega,
+			@RequestParam(required=true) int cantidadDias,
+			@RequestParam(required=true) Date fechaEntrega) throws SQLException {
+    	return new ContratoDao().registrarCronograma(codigoAdenda,numeroEntrega,descripcionEntrega,cantidadDias,fechaEntrega);
+	}
+
+    
+	@RequestMapping(value="/registrarPago",method=RequestMethod.POST)
+	public int registrarPago(@RequestParam(required=true) int codigoAdenda, @RequestParam(required=true) int tipoPago, @RequestParam(required=true) String descripcion) throws SQLException {
+    	return new ContratoDao().registrarPago(codigoAdenda,tipoPago,descripcion);
+	}
+
+    
+	@RequestMapping(value="/registrarPagoDetalle",method=RequestMethod.POST)
+	public int registrarPagoDetalle(@RequestParam(required=true) int codigoAdenda, @RequestParam(required=true) int anio, @RequestParam(required=true) int mes, @RequestParam(required=true) int numeroCuota, @RequestParam(required=true) Date ultimoDiaPago) throws SQLException {
+    	return new ContratoDao().registrarPagoDetalle(codigoAdenda,anio,mes,numeroCuota,ultimoDiaPago);
+	}
+
+	@RequestMapping(value="/consultarContrato",method=RequestMethod.POST)
+	public ConsultarContratoService consultarContrato(@RequestParam(required=true) int codigoAdenda) throws SQLException {
+    	return new ContratoDao().consultarContrato(codigoAdenda);
+	}
+	
+	@RequestMapping(value="/rechazarContrato",method=RequestMethod.POST)
+	public int rechazarContrato(@RequestParam(required=true) int codigoAdenda) throws SQLException {
+    	return new ContratoDao().rechazarContrato(codigoAdenda);
+	}
+
+	@RequestMapping(value="/habilitarContrato",method=RequestMethod.POST)
+	public int habilitarContrato(@RequestParam(required=true) int codigoAdenda) throws SQLException {
+    	return new ContratoDao().habilitarContrato(codigoAdenda);
+	}
+    
+	@RequestMapping(value="/ad_BuscarContrato",method=RequestMethod.POST)
+	public ArrayList<AD_BuscarContratoEntityService> ad_BuscarContrato(@RequestParam(required=true) int codigoCliente) throws SQLException {
+    	return new ContratoDao().ad_BuscarContrato(codigoCliente);
+	}
+    
+	
+	@RequestMapping(value="/ad_ConsultarAdenda",method=RequestMethod.POST)
+	public ConsultarContratoService ad_ConsultarAdenda(@RequestParam(required=true) int codigoAdenda) throws SQLException {
+    	return new ContratoDao().consultarAdenda(codigoAdenda);
+	}
+	
+
+	@RequestMapping(value="/informacionAdenda",method=RequestMethod.POST)
+	public AdendaEntityService informacionAdenda(@RequestParam(required=true) int codigoContrato) throws SQLException {
+    	return new ContratoDao().informacionAdenda(codigoContrato);
+	}
+
+
+	@RequestMapping(value="/registrarAdenda",method=RequestMethod.POST)
+	public int registrarAdenda(
+			@RequestParam(required=true) int codigoContrato,
+			@RequestParam(required=true) int tipoMoneda,
+			@RequestParam(required=true) double monto,
+			@RequestParam(required=true) Date fechaInicio,
+			@RequestParam(required=true) Date fechaFin,
+			@RequestParam(required=true) String dniCliente,
+			@RequestParam(required=true) String dniApoderado,
+			@RequestParam(required=true) String dniGerente,
+			@RequestParam(required=true) int numeroPaginas,
+			@RequestParam(required=true) int numeroClausulas) throws SQLException {
+    	return new ContratoDao().registrarAdenda(codigoContrato,tipoMoneda,monto,fechaInicio,fechaFin,dniCliente,dniApoderado,dniGerente,numeroPaginas,numeroClausulas);
+	}
+	
+	@RequestMapping(value="/buscarAdendas",method=RequestMethod.POST)
+	public ArrayList<ContratoEntityService> buscarAdendas(@RequestParam(required=true) int codigoCliente, @RequestParam(required=true) Date fechaRegistro) throws SQLException {
+    	return new ContratoDao().buscarAdendas(codigoCliente,fechaRegistro);
+	}
+	
+	@RequestMapping(value="/modificarAdenda",method=RequestMethod.POST)
+	public int modificarAdenda(
+			@RequestParam(required=true) int codigoAdenda,
+			@RequestParam(required=true) int tipoMoneda,
+			@RequestParam(required=true) double monto,
+			@RequestParam(required=true) Date fechaInicio,
+			@RequestParam(required=true) Date fechaFin,
+			@RequestParam(required=true) String dniCliente,
+			@RequestParam(required=true) String dniApoderado,
+			@RequestParam(required=true) String dniGerente,
+			@RequestParam(required=true) int numeroPaginas,
+			@RequestParam(required=true) int numeroClausulas) throws SQLException {
+    	return new ContratoDao().modificarAdenda(codigoAdenda,tipoMoneda,monto,fechaInicio,fechaFin,dniCliente,dniApoderado,dniGerente,numeroPaginas,numeroClausulas);
+	}
+
+	@RequestMapping(value="/registrarClausulaImprimirAdenda",method=RequestMethod.POST)
+	public int registrarClausulaImprimirAdenda(@RequestParam(required=true) int codigoAdenda, @RequestParam(required=true) int numeroClausula) throws SQLException {
+    	return new ContratoDao().registrarClausulaImprimirAdenda(codigoAdenda,numeroClausula);
+	}
+	
+	
+	
+	
+	
+		
 }
